035db9bc3cf311f8f46e632f1fd8406a7f6a262d
jcrispino06 <gdkittyowo@gmail.com>
2025-02-26 12:34:32 -0500

    Added bbox data to all data methods

--- earthquake_data.py
+++ earthquake_data.py
@@ -24,22 +24,27 @@ class EarthquakeData:
         url = self.urls["allMonth"]
         request_data = requests.get(url)
         json_data = request_data.json()
-        return json_data["features"]
+        bbox_data = self.get_bbox_data(url)
+        return {"features":json_data["features"],"bbox":bbox_data}
 
     def seven_day_data(self):
         url = self.urls["sevenDay"]
         request_data = requests.get(url)
         json_data = request_data.json()
-        return json_data["features"]
+        bbox_data = self.get_bbox_data(url)
+        return {"features": json_data["features"], "bbox": bbox_data}
 
     def past_day_data(self):
         url = self.urls["pastDay"]
         request_data = requests.get(url)
         json_data = request_data.json()
-        return json_data["features"]
+        bbox_data = self.get_bbox_data(url)
+        return {"features": json_data["features"], "bbox": bbox_data}
 
     def past_hour_data(self):
         url = self.urls["pastHour"]
         request_data = requests.get(url)
         json_data = request_data.json()
-        return json_data["features"]
+        bbox_data = self.get_bbox_data(url)
+        return {"features": json_data["features"], "bbox": bbox_data}
+
